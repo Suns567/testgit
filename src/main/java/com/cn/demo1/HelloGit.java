bf08b8803c967e9ddae1bafcf639a1ea4e26b215
Suns567 <643556627@qq.com>
2021-03-01 13:43:28 +0800

    更新

--- src/main/java/com/cn/demo1/HelloGit.java
+++ src/main/java/com/cn/demo1/HelloGit.java
@@ -4,9 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("HelloGit!");
-		System.out.println("風險");
-
-		System.out.println("創建发展");
+		
 		System.out.println("zhufangf");
 
 		System.out.println("fengz");
